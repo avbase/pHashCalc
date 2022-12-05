01c707a6d9276bf61d70ba75645e7483af784ae0
avicorp <82803116+avbase@users.noreply.github.com>
2022-12-05 13:49:35 +0300

    Release 1.0.1
    Release 1.0.1

--- sample/src/main/java/ru/avicorp/checkphashcalc/MainActivity.kt
+++ sample/src/main/java/ru/avicorp/checkphashcalc/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.avicorp.checkphashcalc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import ru.avicorp.phashcalc.pHashCalc
 
 class MainActivity : AppCompatActivity() {
@@ -16,25 +17,55 @@ class MainActivity : AppCompatActivity() {
         val sourceFileOne = "$aPath/sourceOne.jpg"
         val sourceFileTwo = "$aPath/sourceTwo.jpg"
         val sourceFileThree = "$aPath/sourceThree.jpg"
-
+        val sourceFileFour = "$aPath/sourceFour.bmp"
 
         val phashCalc = pHashCalc()//get to phashCalc library
-        //Сalculate the percentages of identity of two JPEG(JPG) or BMP files (algorithm pHash)
-        if (phashCalc.loadSourceFile(sourceFileOne, sourceFileTwo)) {
-            println("pHash: ${phashCalc.calculateIdentical()}%")
-        } else println("Files not found or check permission.")
 
-        //Condition: files checked and uploaded
+        //Сalculate the percentages of identity of two JPEG(JPG) or(and) BMP files (algorithm pHash)
+        if (phashCalc.loadSourceFile(sourceFileOne, sourceFileFour)) {
+            Log.e("pHashCalc", "pHash: ${phashCalc.calculateIdentical()}%")
+        } else Log.e("pHashCalc", "Files not found or check permission")
+
+        //Condition: files checked and uploaded else clean data
         if (phashCalc.checkCondition()) {
             //Greyscale hash files
-            println("Greyscale hash files: ${phashCalc.getHashOne()} vs ${phashCalc.getHashTwo()}")
+            Log.e("pHashCalc", "Greyscale hash files: ${phashCalc.getHashOne()} vs ${phashCalc.getHashTwo()}")
             //Average pixel greyscale files
-            println("Average pixel greyscale files: ${phashCalc.getAveragePixelOne()} vs  ${phashCalc.getAveragePixelTwo()}")
-        } else println("Please load files")
+            Log.e("pHashCalc", "Average pixel greyscale files: ${phashCalc.getAveragePixelOne()} vs  ${phashCalc.getAveragePixelTwo()}")
+        } else Log.e("pHashCalc", "Please load files")
 
         // Checking the validity of the JPEG(JPG) by its structure (struct is displayed in the log)
-        if (!phashCalc.validJPEGStruct(sourceFileThree)) println("This file is not JPEG(JPG)") else println(
-            "JPEG(JPG) struct OK."
-        )
+        if (!phashCalc.validJPEGStruct(sourceFileThree)) Log.e("JPEG Struct", "This file is not JPEG(JPG)")
+        else Log.e("JPEG Struct", "By JPEG(JPG) signatures, structure is correct")
+
+        //If load BMP, get HEADER else clean data
+        phashCalc.getJPEGData()
+        //If load JPEG, get HEADER else clean data
+        //sizeJPEG - Size of JPEG file (bytes);
+        //howHuffmanTableJPEG - How Huffman Table 1-4
+        //averageByteJPEG - average byte (raw data, not quantization,header,haffman)
+
+
+        // Checking the validity of the BMP by its signatures (header is displayed in the log)
+        if (!phashCalc.validBMPStruct(sourceFileFour)) Log.e("BMP Struct", "This file is not BMP")
+        else Log.e("BMP Struct", "By Bitmap Picture signatures, structure is correct")
+
+        phashCalc.getBMPData()
+        //If load BMP, get HEADER else clean data
+        //sizeBMP -Size of bmp file (bytes);
+        //offsetDataBMP - Offset before image data (bytes);
+        //headerBMP - Size Header Struct;
+        //sizeWidthBMP - Size of Image width (pixels);
+        //sizeHeightBMP - Size of Image height (pixels);
+        //bitDepthBMP - Depth of color: (bit per pixel);
+        //undColDepthBMP - Depth of color: (bit per pixel) (string);
+        //horResInMeterBMP - Horizontal resolution (in pixels per meter);
+        //vertResInMeterBMP - Vertical resolution (in pixels per meter);
+        //numColorImageBMP - Number of colors in the image or zero;
+        //numImpotentColorImageBMP - Number of important colors or zero;
+        //compressBMP - Compression type;
+        //compressTypeBMP - Compression type(string);
+        //sizeRAWImageBMP -Size raw image data (bytes)
+
     }
 }
